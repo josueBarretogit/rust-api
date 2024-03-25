62b2b3d2d69c63e140a54e034b4804539dae6ddc
josueBarretogit <josuebarretogit@gmail.com>
2024-03-25 17:39:15 -0500

    completed median

--- src/helpers/calcs.rs
+++ src/helpers/calcs.rs
@@ -1,5 +1,26 @@
-use std::usize;
 
-pub fn get_avg(arr : &[i32]) -> usize {
+pub fn get_avg(arr : &[i32]) -> f64 {
 
+    if arr.len() <= 0 {
+        return  0.0;
+    }
+
+    let mut sum_elements : i32 = 0;
+
+    for elem in arr.iter() {
+        sum_elements += elem;
+    }
+
+    let avg:  f64 = (sum_elements as f64) / (arr.len() as f64);
+    avg
+
+}
+
+
+pub fn is_odd(num: usize ) -> bool {
+    let remainder  = num % 2;
+    if  remainder >= 1 {
+        return true
+    }
+    false
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,68 @@
 use std::{collections::HashMap, usize};
 
+use helpers::calcs::is_odd;
+
+use crate::helpers::calcs::get_avg;
+
 mod helpers;
 
 fn main() {
 
-    let  list_int = [1,4,5,5,4,7, 152];
+    let   list_int = [1, 3, 4, 2, 6, 5, 8, 7];
 
 
     let median_result = median(&list_int);
 
     println!("median is: {}", median_result);
-    println!("median is: {:?}", list_int);
 
-    println!("mode is: {}", mode_calc(&list_int));
+    println!("average is {}", get_avg(&list_int));
+    println!("Is odd? {}", is_odd(list_int.len()));
+
+
 
 }
 
 
 //first, sort the array
-//second, sum all elemnts
 //if number of elements is odd, the median is the element in the middle
 //if number of elements is even, calculate the avg of the middle elements
-fn median( list_integer: &[i32] ) -> usize {
+fn median( list_integer: &[i32] ) -> f64 {
 
-    let mut co = list_integer.to_owned();
+    if list_integer.len() <= 0 {
+        return 0.0;
+    }
 
-    co.sort();
+    let mut copy_arr = list_integer.to_owned();
 
-    for elem in co.iter().rev() {
+    copy_arr.sort();
 
-        println!("sorted: {}", elem);
+    let arr_length = copy_arr.len();
+    let is_odd = is_odd(arr_length);
+
+    match is_odd {
+        false => {
+
+            let first_middle_num_arr = &copy_arr[0..copy_arr.len() / 2];
+            let second_middle_num_arr = &copy_arr[first_middle_num_arr.len()..];
+
+            let  first_middle_num: &i32 = first_middle_num_arr.last().unwrap();
+            let second_middle_num : &i32   = second_middle_num_arr.first().unwrap();
+
+            println!("{}", first_middle_num);
+            println!("{}", second_middle_num);
+
+            get_avg(&[*first_middle_num, *second_middle_num])
+
+        },
+        true => {
+
+            let middle_num : f64 = copy_arr[0..copy_arr.len() / 2][0].into();
+
+            middle_num
+        }
 
     }
-    return 1 
+ 
 }
 
 fn mode_calc(list_integer: &[i32]) -> i32 {

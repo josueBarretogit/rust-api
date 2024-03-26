e204dc31e6dfb0054e876a2c00834db5afee96a3
josueBarretogit <josuebarretogit@gmail.com>
2024-03-25 21:35:07 -0500

    added mode

--- src/helpers/calcs.rs
+++ src/helpers/calcs.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, i32};
+
 
 pub fn get_avg(arr : &[i32]) -> f64 {
 
@@ -23,4 +25,21 @@ pub fn is_odd(num: usize ) -> bool {
         return true
     }
     false
+}
+
+
+//get the key with the biggest number
+pub fn get_biggest_in_hash(h : &HashMap<String, i32>) -> (String, i32) {
+
+    let mut biggest = 0;
+    let  key  = String::from("key");
+
+    for key_value in h {
+        if *key_value.1 > biggest {
+           biggest = *key_value.1;
+        }
+    }
+
+    (key, biggest)
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-use std::{collections::HashMap, usize};
+#![allow(dead_code)]
+
+use std::{collections::HashMap, i32, pin::pin};
 
 use helpers::calcs::is_odd;
 
-use crate::helpers::calcs::get_avg;
+use crate::helpers::calcs::{get_avg, get_biggest_in_hash};
 
 mod helpers;
 
 fn main() {
 
-    let   list_int = [1, 3, 4, 2, 6, 5, 8, 7];
+    let list_int = [3, 5, 2, 7, 3];
+
 
 
     let median_result = median(&list_int);
 
+    let mode_result = get_mode(&list_int); 
+
+
     println!("median is: {}", median_result);
-
-    println!("average is {}", get_avg(&list_int));
-    println!("Is odd? {}", is_odd(list_int.len()));
-
+    println!("mode is: {}", mode_result);
 
 
 }
@@ -39,35 +42,42 @@ fn median( list_integer: &[i32] ) -> f64 {
     let arr_length = copy_arr.len();
     let is_odd = is_odd(arr_length);
 
+
     match is_odd {
         false => {
+            let index_1 = copy_arr.len() / 2 as usize;
+            let first_middle_num_slice = &copy_arr[0..index_1];
+            let second_middle_num_slice = &copy_arr[first_middle_num_slice.len()..];
 
-            let first_middle_num_arr = &copy_arr[0..copy_arr.len() / 2];
-            let second_middle_num_arr = &copy_arr[first_middle_num_arr.len()..];
+            let  first_middle_num: &i32 = first_middle_num_slice.last().unwrap();
+            let second_middle_num : &i32   = second_middle_num_slice.first().unwrap();
 
-            let  first_middle_num: &i32 = first_middle_num_arr.last().unwrap();
-            let second_middle_num : &i32   = second_middle_num_arr.first().unwrap();
-
-            println!("{}", first_middle_num);
-            println!("{}", second_middle_num);
 
             get_avg(&[*first_middle_num, *second_middle_num])
 
         },
         true => {
 
-            let middle_num : f64 = copy_arr[0..copy_arr.len() / 2][0].into();
+            let middle_num   = copy_arr[0..(copy_arr.len() / 2) + 1].last().unwrap();
 
-            middle_num
+            *middle_num as f64
         }
 
     }
  
 }
 
-fn mode_calc(list_integer: &[i32]) -> i32 {
+fn get_mode(list_integer: &[i32]) -> i32 {
 
-    return list_integer[0]
+    let mut values : HashMap<String, i32> = HashMap::new();
+
+    for  current_value in list_integer.iter(){
+        let current_count = values.entry(current_value.to_string()).or_insert(1);
+        *current_count += 1;
+    }
+
+
+    get_biggest_in_hash(&values).1
 }
 
 

770872e4ced0552093eb1bf83f858123cf6b4e83
josueBarretogit <josuebarretogit@gmail.com>
2024-03-27 11:51:59 -0500

    added sqlx and actix

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,69 @@
-use crate::agrega::agregate::{Summary, Tweet};
+use core::panic;
+use actix_web::{body::BoxBody, get, http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Row};
+use serde::{Serialize, Deserialize};
+use dotenv::dotenv;
 
-mod agrega;
 
-fn main() {
-    let list_int = [1 , 2, 10];
-    let other_list : [i64; 3] = [3,4,23];
 
-    println!("the largest is: {}", find_largest::<i32>(&list_int));
-    println!("the largest is: {}", find_largest::<i64>(&other_list));
-
-    let tweet : Tweet = Tweet { username: "as".to_string(), content: "asd".to_string() , retweet: true, reply: false };
-
-    println!("a twwet: {}" , tweet.summarize(String::from("as")) )
+#[derive(Debug, Serialize, Deserialize)]
+struct Books {
+    title : String
 }
 
-fn find_largest<T : std::cmp::PartialOrd>(arr: &[T]) -> &T {
 
-    let mut biggest = &arr[0];
+impl Responder for Books {
+    type Body = BoxBody;
 
-    for ele in arr {
-        if ele > biggest {
-            biggest = ele
-        }
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
     }
-
-    biggest
 }
+
+
+struct  AppState {
+    db : Pool<Postgres>
+}
+
+
+#[get("/data")]
+async fn get_books(app_data : web::Data<AppState>) -> impl Responder {
+    let db = &app_data.db;
+
+    let res  = sqlx::query("SELECT title from  books where id = 1").fetch_one(db).await.unwrap();
+
+    let book = Books {
+        title : res.get("title")
+    };
+
+    book
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error>  {
+
+    dotenv().ok();
+    let db_uri = std::env::var("DB_URI").unwrap_or_else(|err| panic!("must have a db uri {err}"));
+
+
+    let pg_pool = PgPoolOptions::new().connect(&db_uri).await.unwrap_or_else(|err| panic!("couldnot connect: {err}"));
+
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState {db : pg_pool.clone()}))
+            .service(get_books)
+
+    })
+        .bind(("127.0.0.1" , 8080))?
+        .run()
+    .await
+
+
+}
+
